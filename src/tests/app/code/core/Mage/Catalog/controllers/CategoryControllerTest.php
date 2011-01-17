9fee9f32da795cf78ef1d87327bed83397bc54f4
Alistair Stead <alistair.stead@designdisclosure.com>
2011-01-17 08:16:17 +0000

    added tests for the categories within the system

--- src/tests/app/code/core/Mage/Catalog/controllers/CategoryControllerTest.php
+++ src/tests/app/code/core/Mage/Catalog/controllers/CategoryControllerTest.php
@@ -17,17 +17,80 @@
  * @uses PHPUnit_Framework_Magento_TestCase
  */
 class Mage_Catalog_CategoryControllerTest extends Ibuildings_Mage_Test_PHPUnit_ControllerTestCase {
-
+    
     /**
-     * theIndexActionShouldRedirectToRoot
+     * Member variable that will hold the Category Helper
+     *
+     * @var Mage_Catalog_Helper_Category
+     **/
+    protected $_helperCategory;
+    
+    /**
+     * Setup dependencies for tests
+     *
+     * @return void
+     * @author Alistair Stead
+     **/
+    public function setUp()
+    {
+        parent::setUp();
+        $this->_helperCategory = Mage::helper('catalog/category');
+    }
+    
+    /**
+     * Reset dependencies
+     *
+     * @return void
+     * @author Alistair Stead
+     **/
+    public function tearDown()
+    {
+        parent::tearDown();
+        unset(
+            $this->_helperCategory
+        );
+    }
+    
+    /**
+     * allCategoryURLsShouldRespond
      * @author Alistair Stead
      * @test
      */
-    public function theIndexActionShouldRedirectToRoot()
+    public function allCategoryURLsShouldRespond()
     {
-        $this->markTestIncomplete(
-                  'This test has not been implemented yet.'
+        $categories = $this->_helperCategory->getStoreCategories(true, true, true);
+        
+        foreach ($categories as $key => $category) {
+            if ($this->_helperCategory->canShow($category) && $category->getIsActive()) {
+                // Construct the URL for the category pre dispatch
+                $url = str_replace(Mage::getBaseUrl(), '', $this->_helperCategory->getCategoryUrl($category));
+                var_dump($url);
+                $this->dispatch($url);
+                // Make assertions about the response
+                $this->assertRoute('catalog', "The expected category route has not been matched");
+                $this->assertAction('view', "The index action has not been called");
+                $this->assertController('category', "The expected controller is not been used");
+                // Test the the h1 tag is correctly populated with the category name
+                $this->assertQueryContentContains(
+                    'h1', 
+                    $category->getName(), 
+                    'The page h1 has not been correctly set'
                 );
-    } // theIndexActionShouldRedirectToRoot
+                // Reset after testing the response
+                $this->reset();                
+            } 
+        }
+    } // allCategoryURLsShouldRespond
     
+    /**
+     * invalidCategoryShouldReturn404
+     * @author Alistair Stead
+     * @test
+     */
+    public function invalidCategoryShouldReturn404()
+    {
+        $this->dispatch('catalog/category/view/s/invalid/id/000/');
+        
+        $this->assertResponseCode('404', "The invalid request has not returned 404");
+    } // invalidCategoryShouldReturn404
 }
--- src/tests/app/code/core/Mage/Catalog/controllers/IndexControllerTest.php
+++ src/tests/app/code/core/Mage/Catalog/controllers/IndexControllerTest.php
@@ -8,7 +8,7 @@
  */
 
 /**
- * Mage_Adminhtml_IndexControllerTest
+ * Mage_Catalog_IndexControllerTest
  *
  * @package    Mage_Catalog
  * @subpackage Mage_Catalog_Test
@@ -30,7 +30,10 @@ class Mage_Catalog_IndexControllerTest extends Ibuildings_Mage_Test_PHPUnit_Cont
         $this->assertRoute('cms', "The expected cms route has not been matched");
         $this->assertAction('index', "The index action has not been called");
         $this->assertController('index', "The expected controller is not been used");
-        $this->assertQueryContains('div.nav-container', 'The site navigation is not present on the home page');
+        $this->assertQuery('div.nav-container', 'The site navigation is not present on the home page');
+        
+        $doc = new DomDocument();
+        $doc->loadHTML($this->response->getBody());
         
         // TODO We should capture the navigation here to allow us to browse categories and products
         // TODO Store as a member variable that can be used in other tests
